f9b180e523e92d31376a8dc9d768e1a92196452f
Lin Hui <Lin@HaAir.lan>
2023-11-19 17:02:17 -0500

    main

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ def main():
     X, y = iris.data, iris.target
 
     # Split dataset
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                                    test_size=0.3, random_state=42)
 
     # Train model
     model = RandomForestClassifier(random_state=42)
