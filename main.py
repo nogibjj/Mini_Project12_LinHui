6afafee89264daba009a9255ca51922cc2adaca4
Lin Hui <Lin@HaAir.lan>
2023-11-19 16:56:47 -0500

    first push

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-"""
-Main cli or app entry point
-"""
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-from mylib.calculator import add
-import click
+def main():
+    # Load data
+    iris = load_iris()
+    X, y = iris.data, iris.target
 
-#var=1;var=2
+    # Split dataset
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-@click.command("add")
-@click.argument("a", type=int)
-@click.argument("b", type=int)
-def add_cli(a, b):
-    click.echo(add(a, b))
+    # Train model
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
 
+    # Predict and calculate accuracy
+    predictions = model.predict(X_test)
+    accuracy = accuracy_score(y_test, predictions)
+
+    # MLflow tracking
+    with mlflow.start_run():
+        mlflow.log_param("test_size", 0.3)
+        mlflow.log_metric("accuracy", accuracy)
+        mlflow.sklearn.log_model(model, "model")
 
 if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+    main()
